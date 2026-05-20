1b0e5e1afb3cb9c4da8e70bbd494f750a429d4a5
green_dev_ai <green.joseph@gmail.com>
2026-05-20 16:27:51 +0300

    day 2 challenge

--- week1/day3/DailyChallenge/Day3Challenge.py
+++ week1/day3/DailyChallenge/Day3Challenge.py
@@ -2,16 +2,20 @@
 
 
 word = input("please enter a word :")
+
 my_dict = {
+
 }
 
+
 for (place, char) in enumerate(word):
-#    print(f"{char} , {place}")
+
     if char in my_dict:
-        my_dict.append(place)
+        my_dict[char].append(place)
     else:
-        my_dict[str(char), place] = char, place
-            
+        my_dict[char] = []
+        my_dict[char].append(place)
+          
 print(my_dict)
 
 # --------- Challenge 2
@@ -31,32 +35,40 @@ print(my_dict)
 # Otherwise, print the basket list in alphabetical order.
 # 4. Examples:
 
-basket = []
+
 
 # example 1
-items_purchase = {"Water": "$1", "Bread": "$3", "TV": "$1,000", "Fertilizer": "$20"}
-wallet = "$3,00"
+# items_purchase = {"Water": "$1", "Bread": "$3", "TV": "$1,000", "Fertilizer": "$20"}
+# wallet = "$300"
 
 # exampe 2
-# items_purchase = {"Apple": "$4", "Honey": "$3", "Fan": "$14", "Bananas": "$4", "Pan": "$100", "Spoon": "$2"}
-# wallet = "$100"
+items_purchase = {"Apple": "$4", "Honey": "$3", "Fan": "$14", "Bananas": "$4", "Pan": "$100", "Spoon": "$2"}
+wallet = "$100"
 
 # example 3
 # items_purchase = {"Phone": "$999", "Speakers": "$300", "Laptop": "$5,000", "PC": "$1200"}
 # wallet = "$1"
 
 # data cleaning
-wallet = wallet.replace("$", "")
-wallet = wallet.replace(",", "")
-wallet = int(wallet)
+wallet = int(wallet.replace("$", "").replace(",", ""))
+# print(wallet)
+
 items_purchase_clean = {
-
 }
+basket = []
 
-for item, cost in items_purchase.items():
-    items_purchase_clean[item] = items_purchase[cost].replace("$", "")
-    items_purchase_clean[item] = items_purchase[cost].replace(",", "")
-    items_purchase_clean[item] = int(items_purchase[cost])
-    print(f"{items_purchase[cost + 10000]}")
+for (item, cost) in items_purchase.items():
+    
+    items_purchase_clean[item] = cost
+    items_purchase_clean[item] = int(cost.replace("$", "").replace(",", ""))
 
+    if wallet >= items_purchase_clean[item]:
+        wallet = wallet - items_purchase_clean[item]
+        basket.append(item)
+    
+if len(basket) == 0:
+    print(f"Nothing")
+else:
+    print(f" The items to be purchased are: {sorted(basket)}")
 
+# print(f"you have {wallet} left in your wallet")
